aa4e29e487fa35106924a812cf5809f55103cae0
DogonYu <dorogon01@gmail.com>
2020-12-16 15:21:51 +0900

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import img from 'Static/img/cat.jpg';
+
+const App = () => {
+  return (
+    <div className="container">
+      <div>react App</div>
+      <img src={img} />
+    </div>
+  );
+};
+
+export default App;
