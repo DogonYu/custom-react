1e94c25589fba2cd64d02bd436e39d3625ed10a2
DogonYu <dorogon01@gmail.com>
2020-12-16 16:36:32 +0900

    initial custom react

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import img from 'Static/img/cat.jpg';
 
 const App = () => {
   return (
     <div className="container">
       <div>react App</div>
-      <img src={img} />
     </div>
   );
 };
